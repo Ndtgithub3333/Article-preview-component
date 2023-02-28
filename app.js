9c5e48eb061a130f9e528cd77678a9d3e79d80b3
Thang Nguyen <nguyenducthangg30@gmail.com>
2023-02-28 22:36:10 +0700

    final

---
+++ app.js
@@ -0,0 +1,24 @@
+// const cardShare = document.querySelector('.card__share');
+// const cardContentAuthorImg = document.querySelector('.card__content__author__img');
+// const cardContentAuthorName = document.querySelector('.card__content__author__name');
+// const cardContentShareList = document.querySelector('.card__content__share__list');
+
+
+
+
+
+
+// // cardShare.addEventListener('mouseover', function () {
+// //     cardContentShareList.classList.remove('hidden');
+// // });
+
+// // cardShare.addEventListener('mouseout', function () {
+// //     cardContentShareList.classList.add('hidden');
+// // });
+
+
+// cardShare.addEventListener('click', function () {
+//     cardContentAuthorImg.classList.add('hidden');
+//     cardContentAuthorName.classList.add('hidden');
+//     cardContentShareList.classList.add('hidden');
+// });
